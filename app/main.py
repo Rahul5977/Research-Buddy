8c398c794a60d4a2211dd813fa99659181e3341b
Rahul Raj <rahul.raj9237@gmail.com>
2025-07-30 10:41:12 +0530

    post/process-document

--- app/main.py
+++ app/main.py
@@ -1,1 +1,16 @@
 # fastapi app
+from fastapi import FastAPI,UploadFile,File
+from dotenv import load_dotenv
+load_dotenv()
+
+app=FastAPI(title="Research Buddy")
+
+@app.get("/")
+def read_root():
+    return {"message":"Welcome to AI research assistant"}
+
+# endpoint for uploading files
+@app.post("/porcess-documents")
+async def process_document(file:UploadFile=File(...)):
+    return {"filename":file.filename,"content-type":file.content_type}
+    
