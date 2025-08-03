225129a818abccfa9dc4bf0aff1b0423402f6414
Rahul Raj <rahul.raj9237@gmail.com>
2025-08-03 13:09:39 +0530

    citations ->visualizer->compileBooklet-node

--- app/graphs/processing_nodes.py
+++ app/graphs/processing_nodes.py
@@ -3,11 +3,14 @@ from ..utils import parser
 from langchain_community.vectorstores import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
-from agents import summarizer
+from agents import summarizer, citations, visualizer, pdf_compiler
 def parse_document_node(state: GraphState)->GraphState:
     print("---NODE: Parsing Document---")
-    state["parsed_data"] = parser.parse_pdf_with_grobid(state["new_document_path"])
-    state["new_document_path"] = None
+    try:
+        path=state["new_document_path"]
+        state["parsed_data"] = parser.parse_document(path)
+    except Exception as e:
+       state["error_message"] = f"Failed during PDF parsing: {e}"
     return state
 
 def chunk_and_embed_node(state: GraphState)->GraphState:
@@ -28,4 +31,35 @@ def summary_node(state: GraphState)->GraphState:
     full_text=state["parsed_data"].get("full_text","")
     summary=summarizer.summarize_text(full_text)
     state["summary"] = summary
+    return state
+
+def citations(state: GraphState)->GraphState:
+    print("---NODE: Creating Citations---")
+    try:
+        state["citations"] = citations.citations(state["summary"])
+    except Exception as e:
+       state["error_message"] = f"failed to create citations: {e}"
+    return state
+
+def visualizer(state: GraphState)->GraphState:
+    print("---NODE: Creating Visualizer---")
+    try:
+        state["diagram_dotcode"] = visualizer.create_diagram_from_text(state["parsed_data"]["full_text"])
+    except Exception as e:
+       state["error_message"] = f"Failed during visualizer: {e}"
+    return state
+    
+    
+def compile_booklet_node(state: GraphState) -> GraphState:
+    print("---NODE: Compiling PDF Booklet---")
+    try:
+        pdf_path = pdf_compiler.compile_booklet_from_data(
+            job_id=state["job_id"],
+            summary=state["summary"],
+            citations=state["citations"],
+            dot_code=state["diagram_dot_code"]
+        )
+        state["booklet_path"] = pdf_path
+    except Exception as e:
+        state["error_message"] = f"Failed during PDF compilation: {e}"
     return state
